fc2a42c6bce467149a579e7aadfcebdbe77e9dad
vt5491 <vt5491@gmail.com>
2018-08-22 23:41:35 -0700

    development changes.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pure_pitch
 purescript tone generator
+
+# to run under browser:
+1) cd to root dir of pure_pitch  
+2) pulp build && pulp browserify > index.js  
+3) pulp server  --host 0.0.0.0  
+ Note: --host is only needed if you will run from a different machine  
+4) http://localhost:1337/
--- src/PurePitch.purs
+++ src/PurePitch.purs
@@ -6,18 +6,19 @@ import Data.List.Types
 import Data.Maybe
 import Data.String
 import Prelude
+import Math (pow)
 
 import Audio.WebAudio.AudioParam (getValue, setValue, setValueAtTime)
 import Audio.WebAudio.BaseAudioContext (createGain, createOscillator, currentTime, destination, newAudioContext, resume, state, suspend)
 import Audio.WebAudio.GainNode (gain)
 import Audio.WebAudio.Oscillator (OscillatorType(..), frequency, setFrequency, setOscillatorType, startOscillator, stopOscillator)
 import Audio.WebAudio.Types (AudioContext, GainNode, OscillatorNode, AudioContextState(..), connect)
-import Data.Array (filter, (..), (!!), index, length, toUnfoldable, fromFoldable)
+import Data.Array (filter, (..), (!!), index, length, toUnfoldable, fromFoldable, concat, concatMap, cons, snoc)
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
 import Data.Int (toNumber, fromNumber, ceil)
 import Data.List as L
 import Data.Newtype (over, wrap)
-import Data.Generic.Rep (class Generic)
-import Data.Generic.Rep.Show (genericShow)
 import Effect (Effect)
 import Effect.Console (log)
 import Effect.Timer as T
@@ -32,6 +33,24 @@ import Web.HTML.HTMLDocument (toParentNode, toDocument)
 import Web.HTML.HTMLElement (toElement, fromElement, className, HTMLElement)
 import Web.HTML.Window (Window, document)
 
+halfStepMultiple :: Number
+halfStepMultiple = 1.059463
+
+-- Assume this is relative to A (e.g. 440 hz)
+chromaticLookup :: Int -> Number
+chromaticLookup 0  = 1.000000
+chromaticLookup 1  = 1.059463
+chromaticLookup 2  = 1.122462
+chromaticLookup 3 = 1.189207
+chromaticLookup 4 = 1.259921
+chromaticLookup 5 = 1.334839
+chromaticLookup 6 = 1.414213
+chromaticLookup 7 = 1.498307
+chromaticLookup 8 = 1.587401
+chromaticLookup 9 = 1.681792
+chromaticLookup 10 = 1.781797
+chromaticLookup 11 = 1.887748
+chromaticLookup _ = 1.00
 
 chromaticMap :: ToneIndex -> Number
 chromaticMap Zero  = 1.000000
@@ -78,23 +97,6 @@ derive instance ordToneIndex :: Ord ToneIndex
 wholeNoteDuration :: Number
 wholeNoteDuration = 1.0
 
--- type Entry =
---   { firstName :: String
---   , lastName  :: String
---   , address   :: Address
---   }
--- type NoteType =
---   { freq :: Number,
---     durRatio :: Int
---   }
---
--- data NoteData = NoteData
---   { freq :: Number
---   , durRatio :: Int
---   }
-
--- data Note = Note { freq :: Number , durRatio :: Int }
-
 -- showNote :: NoteType -> String
 showNote2 :: Note -> String
 -- showNote n = "note.freq=" <> show n.freq
@@ -108,38 +110,46 @@ getFreq (Note {freq, durRatio}) = freq
 getDurRatio :: Note -> Int
 getDurRatio (Note {freq, durRatio}) = durRatio
 
--- instance showList :: Show a => Show (List a) where
---   show x = genericShow x
--- instance showNote :: Show a => Show (Note a) where
---   show x = showNote2 x
+getString :: GuitarNote -> Int
+getString (GuitarNote {string, fret}) = string
 
--- instance showMeasurementId :: Show MeasurementId where
---   show = gShow
--- following works:
--- instance showNote :: Show Note where
---   show = showNote2
-
--- instance showMyRecord :: Show MyRecord where
---   show = gShow
--- derive instance genericMyRecord :: Generic MyRecord
--- newtype MyRecord = MyRecord { a :: Int }
--- derive instance genericNote :: Generic Note
--- derive instance genericPerson :: Generic Person _
 newtype Note = Note { freq :: Number , durRatio :: Int }
 derive instance genericNote :: Generic Note _
 instance showNote :: Show Note where
   show = genericShow
 
--- showNoteData :: NoteData -> String
--- showNoteData n = "note.freq=" <> show n.freq
-  -- type AddressBook = List Entry
+newtype GuitarNote = GuitarNote {string :: Int, fret :: Int }
+derive instance genericGuitarNote :: Generic GuitarNote _
+instance showGuitarNote :: Show GuitarNote where
+  show = genericShow
 
--- newtype A = A {x::Int, y::Int}
--- newtype N2 = N2 {x:: Int, y:: Int}
--- overTheMountianTriads
--- type AddressBook = List Entry
--- type Tune = Array Note
--- type Tune = Array NoteData
+newtype Stanza = Stanza {repeat :: Int, notes :: Array GuitarNote}
+derive instance genericStanza :: Generic Stanza _
+instance showStanza :: Show Stanza where
+  show = genericShow
+
+getStanza :: Array Stanza -> Int -> Stanza
+getStanza a n = do
+  let ms = a !! n
+  case ms of
+    Nothing -> Stanza {repeat: 0, notes: []}
+    Just s -> s
+
+getStanzaNotes :: Stanza -> Array GuitarNote
+getStanzaNotes (Stanza {repeat, notes}) = notes
+
+getStanzaRepeat :: Stanza -> Int
+getStanzaRepeat (Stanza {repeat, notes}) = repeat
+
+-- getStanzaNotes :: Maybe Stanza -> Array GuitarNote
+-- getStanzaNotes ms =  case ms of
+--                       Nothing -> []
+--                       Just s -> do
+--                         let mNotes = s
+
+-- newtype Song = (Array Note) | (Array GuitarNote)
+-- type Song =  Note {freq :: Number, durRatio :: Int} | GuitarNote {string :: Int, fret :: Int}
+-- type Song = (Array Note) | (Array GuitarNote)
 
 -- overTheMountianTriads :: Tune
 -- overTheMountianTriads :: Array {freq :: Number, durRatio :: Int }
@@ -156,17 +166,54 @@ overTheMountianTriads = [
 
 otm = overTheMountianTriads
 
--- emptyTune :: Tune
--- emptyTune = empty
+overTheMountianTab :: Array GuitarNote
+overTheMountianTab = [
+  GuitarNote {string: 1, fret: 17},
+  GuitarNote {string: 1, fret: 13},
+  GuitarNote {string: 2, fret: 15},
 
--- showAddress :: Address -> String
--- showAddress addr = addr.street <> ", " <>
--- showNote n =  show n.freq
--- instance showRomanDigit :: Show RomanDigit where
---   -- show x = "Foo bar=" <> show x
---   show M = "M"
--- instance showNote :: Show Note where
---   show n = "note.freq=" <> n.freq
+  GuitarNote {string: 1, fret: 18},
+  GuitarNote {string: 1, fret: 13},
+  GuitarNote {string: 2, fret: 15},
+
+  GuitarNote {string: 1, fret: 15},
+  GuitarNote {string: 1, fret: 12},
+  GuitarNote {string: 2, fret: 13},
+
+  GuitarNote {string: 1, fret: 17},
+  GuitarNote {string: 1, fret: 13},
+  GuitarNote {string: 2, fret: 13}
+  ]
+
+otmTab = overTheMountianTab
+
+otmTab2 :: Array Stanza
+otmTab2 = [
+  Stanza {repeat: 4, notes: [
+    GuitarNote {string: 1, fret: 17},
+    GuitarNote {string: 1, fret: 13},
+    GuitarNote {string: 2, fret: 15}
+  ]},
+  Stanza {repeat: 4, notes: [
+    GuitarNote {string: 1, fret: 18},
+    GuitarNote {string: 1, fret: 13},
+    GuitarNote {string: 2, fret: 15}
+  ]}
+  ]
+
+-- Stanza {repeat: 3,
+--   [
+--     GuitarNote {string: 1, fret: 17},
+--     GuitarNote {string: 1, fret: 13},
+--     GuitarNote {string: 2, fret: 15}
+--   ]
+-- }
+
+-- otmTab2 ::
+
+globalSong = otmTab
+globalSongLen = (length globalSong) - 0
+
 myIntList :: List Int
 myIntList = (Cons 1 Nil)
 
@@ -210,38 +257,13 @@ beep freq = do
   stopOscillator duration osc
   pure unit
 
--- playTune :: Tune -> Effect Unit
--- playTune :: Array {freq :: Number, durRatio :: Int} -> Effect Unit
--- playTuneOld :: Array Note -> Effect Unit
--- playTuneOld tune = do
+-- playTune2 :: Array Note -> Effect Unit
+-- playTune2 tune = do
 --   ctx <- newAudioContext
 --   osc <- createOscillator ctx
---   startOscillator 0.0 osc
---   let mn = otm !! 1
---   -- let abc :: Number
---   -- let abc = 7.0
---   let freq  =  case mn of
---                 Nothing -> 0.0
---                 -- Just n -> debugger 1
---                 -- Just n -> fromNumber $ 240
---                 -- Just n -> 240
---                 -- Just n -> ceil n.freq
---                 -- Just n -> ceil $ getFreq n
---                 Just n ->  getFreq n
---   -- let freq = mn do
---   --                 case mn of
---   --                   Nothing -> 0
---   --                   Just n -> n.freq
---   log $ "playTune: freq=" <> show freq
---   setValue freq =<< frequency osc
 --   connect osc =<< destination ctx
---   let duration = 0.5
---   -- halfway through, double the freq
---   -- void $ T.setTimeout $ duration / 2.0 do
---   --   setFrequency freq  osc
---   _ <- T.setTimeout 250 do
---     setFrequency 1200.0  osc
---   stopOscillator duration osc
+--   startOscillator 0.0 osc
+--   -- foldl
 --   pure unit
 
 playTune :: Array Note -> Effect Unit
@@ -250,36 +272,58 @@ playTune tune = do
   osc <- createOscillator ctx
   connect osc =<< destination ctx
   startOscillator 0.0 osc
-  playNote osc 0
+  -- playNote osc 0
+  -- playNote2 osc globalSong 0
+  playStanza osc $ getStanza otmTab2 1
   pure unit
 
--- toneStream :: Array Note -> Effect Unit
--- playNote :: Effect OscillatorNode ->  Int -> Effect Unit
 playNote :: OscillatorNode ->  Int -> Effect Unit
-playNote osc 3 = stopOscillator 0.0  osc
-playNote osc i = do
-  let mNote = otm !! i
-  let freq  = case mNote of
-                Nothing -> 0.0
-                Just n ->  getFreq n
-  log $ "playNote: freq=" <> show freq
-  setValue freq =<< frequency  osc
-  -- let duration = 0.5
-  _ <- T.setTimeout 500 do
-    -- setFrequency 1200.0  osc
-      -- let mNote = otm !! i
-      playNote osc (i + 1)
+-- playNote osc 12 = stopOscillator 0.0  osc
+-- playNote osc n |songLen = stopOscillator 0.0  osc
+  -- where songLen = length globalSong
+  -- where songLen = 6
+playNote osc i
+  -- let songLen = 12
+  -- in
+  | i > globalSongLen = log "playNote: error"
+  | i == globalSongLen = stopOscillator 0.0  osc
+  | i < globalSongLen = do
+    let mNote = mTabToNote $ otmTab !! i
+    let freq  = case mNote of
+                  Nothing -> 0.0
+                  Just n ->  getFreq n
+    log $ "playNote: freq=" <> show freq
+    setValue freq =<< frequency  osc
+    _ <- T.setTimeout 500 do
+        playNote osc (i + 1)
 
-  pure unit
+    pure unit
+playNote _ _ = log "playNote: fallthrough"
 
--- initOscillator :: Effect Unit
--- initOscillator = do
---   ctx <- newAudioContext
---   osc <- createOscillator ctx
+playNote2 :: OscillatorNode -> Array GuitarNote ->  Int -> Effect Unit
+playNote2 osc notes i
+    | i > length notes = log "playNote2: error"
+    | i == (length notes) = stopOscillator 0.0  osc
+    | i < (length notes) = do
+      let mNote = mTabToNote $ notes !! i
+      let freq  = case mNote of
+                    Nothing -> 0.0
+                    Just n ->  getFreq n
+      log $ "playNote: freq=" <> show freq
+      setValue freq =<< frequency  osc
+      _ <- T.setTimeout 500 do
+          playNote2 osc notes (i + 1)
 
--- play a tone stream (e.g. a lick or little note sequence)
--- toneStream :: Number -> Effect Unit
--- toneStream freq = do
+      pure unit
+
+playNote2 _ _ _ = log "playNote2: fallthrough"
+
+playStanza :: OscillatorNode -> Stanza -> Effect Unit
+playStanza o s = playNote2 o (getStanzaNotes s)  0
+
+-- create an array of GuitarNote's from a Stanza Array
+-- flattenStanzas :: Array Stanza -> Array GuitarNote
+-- flattenStanzas a =
 
 setupPlayBtn ::  Effect Unit
 setupPlayBtn = do
@@ -303,20 +347,8 @@ setupPlayBtn = do
             evtListener
             false
             ((toEventTarget <<< toElement) htmlEl)
-          -- vtEvtListener <- (eventListener vtEvtHandler)
-          -- addEventListener
-          --   (EventType "vtEvt")
-          --   vtEvtListener
-          --   false
-          --   ((toEventTarget <<< toElement) htmlEl)
-          -- pure unit
       pure unit
   pure unit
-
--- btnHandler :: Event -> Effect Unit
--- btnHandler e = do
---   log "btn pressed"
---   beep 890.0
 
 startTone :: Event -> Effect Unit
 startTone e = do
@@ -326,6 +358,36 @@ startTone e = do
   playTune otm
   pure unit
 
+fretToFreq :: GuitarNote -> Number
+-- fretToFreq (GuitarNote {string, fret}) = toNumber (string + fret)
+fretToFreq (GuitarNote {string, fret}) =
+                      case string of
+                        6 -> 82.41 * pow halfStepMultiple (toNumber fret)
+                        5 -> 110.0 * pow halfStepMultiple (toNumber fret)
+                        4 -> 146.83 * pow halfStepMultiple (toNumber fret)
+                        3 -> 196.0 * pow halfStepMultiple (toNumber fret)
+                        2 -> 246.94 * pow halfStepMultiple (toNumber fret)
+                        1 -> 329.63 * pow halfStepMultiple (toNumber fret)
+                        _ -> 1.0
+
+tabToNote :: GuitarNote -> Note
+tabToNote tab = Note {freq: fretToFreq tab, durRatio: 12}
+
+mTabToNote :: Maybe GuitarNote -> Maybe Note
+-- mTabToNote (GuitarNote {string, fret}) = case mtab of
+mTabToNote mtab = case mtab of
+                    Nothing -> Nothing
+                    Just gn -> Just $ Note {freq: fretToFreq gn, durRatio: 12}
+
+-- repeatGuitarNotes :: Array GuitarNote -> Int -> Array GuitarNote
+-- repeatGuitarNotes xs 0 = xs
+-- repeatGuitarNotes xs n = a
+
+-- repeatIntArray :: Array Int -> Int -> Array Int
+-- repeatIntArray xs 0 = xs
+-- repeatIntArray xs n =
+
+-- typed hole example
 -- arrayToList :: forall a. Array a -> List a
 -- arrayToList = ?whatGoesHere
 
